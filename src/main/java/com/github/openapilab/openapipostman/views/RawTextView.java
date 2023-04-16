627725112643804b03cf1a984544a552bf1fd0fa
Beppe Catanese <gcatanese@yahoo.com>
2023-04-16 22:59:06 +0200

    Introduce superclass

--- src/main/java/com/github/openapilab/openapipostman/views/RawTextView.java
+++ src/main/java/com/github/openapilab/openapipostman/views/RawTextView.java
@@ -3,16 +3,11 @@ package com.github.openapilab.openapipostman.views;
 import com.github.openapilab.openapipostman.ApplicationProperty;
 import com.github.openapilab.openapipostman.exception.BaseException;
 import com.github.openapilab.openapipostman.model.OpenApiSpec;
-import com.github.openapilab.openapipostman.service.*;
+import com.github.openapilab.openapipostman.service.FileService;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -26,30 +21,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.io.IOException;
 
 @Route(value = "rawtext", layout = MainLayout.class)
-public class RawTextView extends VerticalLayout {
+public class RawTextView extends GeneratorView {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RawTextView.class);
 
-    // constructor injection
-    private ApplicationProperty applicationProperty;
-    private FileService fileService;
-
-    // field injection
-    @Autowired
-    private GeneratorService generatorService;
-    @Autowired
-    private UrlService urlService;
-    @Autowired
-    private JsonService jsonService;
-    @Autowired
-    private YamlService yamlService;
-
-
     @Autowired
     public RawTextView(ApplicationProperty applicationProperty, FileService fileService) throws Exception {
-
-        this.applicationProperty = applicationProperty;
-        this.fileService = fileService;
+        super(applicationProperty, fileService);
 
         setSizeFull();
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
@@ -131,66 +109,5 @@ public class RawTextView extends VerticalLayout {
         return layout;
     }
 
-    String process(OpenApiSpec openApiSpec) throws Exception {
 
-        if(jsonService.isJson(openApiSpec.getRaw())) {
-            if(!jsonService.isValidJson(openApiSpec.getRaw())) {
-                throw new BaseException("Invalid JSON");
-            }
-
-            // force OpenAPI version
-            openApiSpec.setRaw(jsonService.setOpenApiVersion(openApiSpec.getRaw()));
-        }
-
-        if(yamlService.isValidYaml(openApiSpec.getRaw())) {
-            if(!yamlService.isValidYaml(openApiSpec.getRaw())) {
-                throw new BaseException("Invalid YAML");
-            }
-        }
-
-        String input = fileService.save(openApiSpec.getRaw());
-        String postmanFile = generatorService.generate(input);
-
-        return postmanFile;
-
-    }
-
-    Notification setNotification(String message) {
-        Notification notification = new Notification();
-        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
-        notification.setPosition(Notification.Position.TOP_CENTER);
-        notification.setDuration(5000);
-
-        Div text = new Div(new Text(message));
-
-        Button closeButton = new Button(new Icon("lumo", "cross"));
-        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
-        closeButton.getElement().setAttribute("aria-label", "Close");
-        closeButton.addClickListener(event -> {
-            notification.close();
-        });
-
-        HorizontalLayout messageLayout = new HorizontalLayout(text, closeButton);
-        messageLayout.setAlignItems(Alignment.CENTER);
-
-        notification.add(messageLayout);
-        notification.open();
-
-        return notification;
-    }
-
-    private ReCaptcha initReCaptcha() {
-        return new ReCaptcha(
-                applicationProperty.getRecaptchaSiteKey(),
-                applicationProperty.getRecaptchaSecretKey()
-        );
-    }
-
-    private Component footer() {
-        Div footer = new Div();
-        footer.getElement().getStyle().set("font-size", "14px");
-        footer.setText("Made with ❤️, OpenAPI Generator and Vaadin");
-
-        return footer;
-    }
 }
--- src/main/java/com/github/openapilab/openapipostman/views/UrlView.java
+++ src/main/java/com/github/openapilab/openapipostman/views/UrlView.java
@@ -3,16 +3,12 @@ package com.github.openapilab.openapipostman.views;
 import com.github.openapilab.openapipostman.ApplicationProperty;
 import com.github.openapilab.openapipostman.exception.BaseException;
 import com.github.openapilab.openapipostman.model.OpenApiSpec;
-import com.github.openapilab.openapipostman.service.*;
+import com.github.openapilab.openapipostman.service.FileService;
+import com.github.openapilab.openapipostman.service.UrlService;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -24,29 +20,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value="", layout = MainLayout.class)
-public class UrlView extends VerticalLayout {
+public class UrlView extends GeneratorView {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UrlView.class);
 
     // constructor injection
-    private ApplicationProperty applicationProperty;
     private FileService fileService;
 
     // field injection
     @Autowired
-    private GeneratorService generatorService;
-    @Autowired
     private UrlService urlService;
-    @Autowired
-    private JsonService jsonService;
-    @Autowired
-    private YamlService yamlService;
 
     @Autowired
     public UrlView(ApplicationProperty applicationProperty, FileService fileService) throws Exception {
-
-        this.applicationProperty = applicationProperty;
-        this.fileService = fileService;
+        super(applicationProperty, fileService);
 
         setSizeFull();
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
@@ -60,54 +47,6 @@ public class UrlView extends VerticalLayout {
         add(layout);
         add(footer());
 
-    }
-
-    String process(OpenApiSpec openApiSpec) throws Exception {
-
-        if(jsonService.isJson(openApiSpec.getRaw())) {
-            if(!jsonService.isValidJson(openApiSpec.getRaw())) {
-                throw new BaseException("Invalid JSON");
-            }
-
-            // force OpenAPI version
-            openApiSpec.setRaw(jsonService.setOpenApiVersion(openApiSpec.getRaw()));
-        }
-
-        if(yamlService.isValidYaml(openApiSpec.getRaw())) {
-            if(!yamlService.isValidYaml(openApiSpec.getRaw())) {
-                throw new BaseException("Invalid YAML");
-            }
-        }
-
-        String input = fileService.save(openApiSpec.getRaw());
-        String postmanFile = generatorService.generate(input);
-
-        return postmanFile;
-
-    }
-
-    Notification setNotification(String message) {
-        Notification notification = new Notification();
-        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
-        notification.setPosition(Notification.Position.TOP_CENTER);
-        notification.setDuration(5000);
-
-        Div text = new Div(new Text(message));
-
-        Button closeButton = new Button(new Icon("lumo", "cross"));
-        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
-        closeButton.getElement().setAttribute("aria-label", "Close");
-        closeButton.addClickListener(event -> {
-            notification.close();
-        });
-
-        HorizontalLayout messageLayout = new HorizontalLayout(text, closeButton);
-        messageLayout.setAlignItems(Alignment.CENTER);
-
-        notification.add(messageLayout);
-        notification.open();
-
-        return notification;
     }
 
     private Component url() throws Exception {
@@ -177,18 +116,4 @@ public class UrlView extends VerticalLayout {
         return layout;
     }
 
-    private ReCaptcha initReCaptcha() {
-        return new ReCaptcha(
-                applicationProperty.getRecaptchaSiteKey(),
-                applicationProperty.getRecaptchaSecretKey()
-        );
-    }
-
-    private Component footer() {
-        Div footer = new Div();
-        footer.getElement().getStyle().set("font-size", "14px");
-        footer.setText("Made with ❤️, OpenAPI Generator and Vaadin");
-
-        return footer;
-    }
 }
