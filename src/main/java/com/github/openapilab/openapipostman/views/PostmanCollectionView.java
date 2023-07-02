21766d0fcff60bc215190da8dee0a85123b674da
Beppe Catanese <gcatanese@yahoo.com>
2023-07-02 22:57:15 +0200

    Add push to Postman dialog

--- src/main/java/com/github/openapilab/openapipostman/views/PostmanCollectionView.java
+++ src/main/java/com/github/openapilab/openapipostman/views/PostmanCollectionView.java
@@ -4,7 +4,10 @@ import com.github.openapilab.openapipostman.service.FileService;
 import com.github.openapilab.openapipostman.service.JsonService;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -61,6 +64,7 @@ public class PostmanCollectionView extends VerticalLayout {
         Button copyButton = new Button("Copy", copyIcon);
         copyButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
         ClipboardHelper clipboardHelper = new ClipboardHelper(textArea.getValue(), copyButton);
+
         // Save button
         Icon downloadIcon = VaadinIcon.DOWNLOAD.create();
         downloadIcon.setTooltipText("Download");
@@ -71,10 +75,24 @@ public class PostmanCollectionView extends VerticalLayout {
                 new StreamResource("postman.json", () -> new ByteArrayInputStream(textArea.getValue().getBytes())));
         buttonWrapper.wrapComponent(downloadButton);
 
+        // Push button
+        Icon pushIcon = VaadinIcon.SHARE.create();
+        copyIcon.setTooltipText("Push to Postman with curl");
+
+        Button pushButton = new Button("Push", pushIcon);
+        pushButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        // open dialog on click
+        pushButton.addClickListener(e -> {
+            Dialog dialog = createPushDialog();
+            add(dialog);
+            dialog.open();
+        });
+
         HorizontalLayout buttonLayout = new HorizontalLayout(clipboardHelper);
         buttonLayout.setWidthFull();
         buttonLayout.setJustifyContentMode(JustifyContentMode.END);
         buttonLayout.add(buttonWrapper);
+        buttonLayout.add(pushButton);
 
         HorizontalLayout textAreaHeading = new HorizontalLayout();
         textAreaHeading.setWidthFull();
@@ -91,15 +109,59 @@ public class PostmanCollectionView extends VerticalLayout {
         layout.add(textAreaHeading);
         layout.add(textArea);
 
-        Icon homeIcon = VaadinIcon.HOME.create();
-
-        Button homeButton = new Button("Home", homeIcon);
-        homeButton.addClickListener(ev -> {
-                    homeButton.getElement().executeJs("window.location.href = '/' ");
-                });
+        Anchor homeAnchor = new Anchor("/", VaadinIcon.HOME.create());
+        homeAnchor.getElement().setAttribute("router-ignore", "");
 
         add(layout);
-        add(homeButton);
+        add(homeAnchor);
+    }
+
+    private Dialog createPushDialog() {
+        // CURL command
+        String command = "curl -X POST -d \"{\\\"collection\\\": $(<postman.json) }\" https://api.getpostman.com/collections --header 'Content-Type: application/json' --header 'X-API-Key: MY_POSTMAN_KEY'";
+
+        Dialog dialog = new Dialog();
+        dialog.setWidth("50%");
+        dialog.setHeaderTitle("Push to Postman");
+
+        VerticalLayout dialogLayout = new VerticalLayout();
+        dialogLayout.setPadding(false);
+        dialogLayout.setSpacing(false);
+
+        Paragraph p1 = new Paragraph();
+        p1.setText("Save the postman.json file and open the terminal in the same folder. \n" +
+                "Copy the command below, replace with your Postman KEY and run with curl.");
+        p1.getStyle().set("white-space", "pre-line");
+        dialogLayout.add(p1);
+
+        // Command copy button
+        Icon commandCopyIcon = VaadinIcon.COPY.create();
+        commandCopyIcon.setTooltipText("Copy");
+        Button commandCopyButton = new Button("Copy", commandCopyIcon);
+        commandCopyButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        ClipboardHelper commandClipboardHelper = new ClipboardHelper(command, commandCopyButton);
+
+        HorizontalLayout buttonLayout = new HorizontalLayout(commandClipboardHelper);
+        buttonLayout.setWidthFull();
+        buttonLayout.setJustifyContentMode(JustifyContentMode.END);
+        dialogLayout.add(buttonLayout);
+
+        TextArea curlCommand = new TextArea();
+        curlCommand.setReadOnly(true);
+        curlCommand.setWidthFull();
+        curlCommand.setMinHeight("100px");
+        curlCommand.setValue(command);
+        dialogLayout.add(curlCommand);
+
+        dialog.add(dialogLayout);
+
+        Button closeButton = new Button(new Icon("lumo", "cross"),
+                (ev) -> dialog.close());
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        dialog.getHeader().add(closeButton);
+
+        return dialog;
+
     }
 
 }
